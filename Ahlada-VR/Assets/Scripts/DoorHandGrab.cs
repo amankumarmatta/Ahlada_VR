c00734bcef2ccc8b436bbfad57539a41a0a39f93
Aman K <amankumarmatta0@gmail.com>
2025-09-27 23:36:27 +0530

    Added Values Logic

--- Ahlada-VR/Assets/Scripts/DoorHandGrab.cs
+++ Ahlada-VR/Assets/Scripts/DoorHandGrab.cs
@@ -1,35 +1,30 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-
 public class DoorHandleGrab : UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable
 {
-    [SerializeField] private Transform door;   // The actual door object
-
+    [SerializeField] private Transform door;   
+    [SerializeField] private float minY = -90f; 
+    [SerializeField] private float maxY = 90f;  
     private Quaternion initialGrabRotation;
     private Quaternion initialDoorRotation;
-
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
-
         initialGrabRotation = args.interactorObject.transform.rotation;
         initialDoorRotation = door.rotation;
     }
-
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
     {
         base.ProcessInteractable(updatePhase);
-
         if (isSelected && firstInteractorSelecting != null)
         {
             Quaternion currentGrabRotation = firstInteractorSelecting.transform.rotation;
             Quaternion delta = currentGrabRotation * Quaternion.Inverse(initialGrabRotation);
-
             Quaternion targetRotation = delta * initialDoorRotation;
-
-            // 🔒 Lock X and Z, allow only Y
-            Vector3 euler = targetRotation.eulerAngles;
-            door.rotation = Quaternion.Euler(0f, euler.y, 0f);
+            float rawY = targetRotation.eulerAngles.y;
+            float y = Mathf.DeltaAngle(0f, rawY);
+            y = Mathf.Clamp(y, minY, maxY);
+            door.rotation = Quaternion.Euler(0f, y, 0f);
         }
     }
 }
