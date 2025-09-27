cbca68550f60d3d87a5610dfe81bad14c0da44cd
Aman K <amankumarmatta0@gmail.com>
2025-09-28 04:57:55 +0530

    Added Button Click and Hover Sounds, Adjusted the position of the buttons on the walls in Intro and Tutorial Scenes

---
+++ Ahlada-VR/Assets/Scripts/UISounds.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UISounds : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    public AudioClip highlightSound;
+    public AudioClip clickSound;
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        // Get or create AudioSource
+        _audioSource = FindFirstObjectByType<AudioSource>();
+        if (_audioSource == null)
+        {
+            GameObject audioObj = new GameObject("UIAudioSource");
+            _audioSource = audioObj.AddComponent<AudioSource>();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (highlightSound != null)
+            _audioSource.PlayOneShot(highlightSound);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clickSound != null)
+            _audioSource.PlayOneShot(clickSound);
+    }
+}
