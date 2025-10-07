117abd779e5da4191e13a0f8226b3ff012a85fbc
Aman K <amankumarmatta0@gmail.com>
2025-10-07 23:47:38 +0530

    Added Exit Buttons and Back Button in Tutorial Scene

--- Ahlada-VR/Assets/Scripts/UIController.cs
+++ Ahlada-VR/Assets/Scripts/UIController.cs
@@ -3,23 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-    [Header("Scene References")]
-    public string skipSceneName;
-    public string exploreSceneName;
+  [Header("Scene References")]
+  public string skipSceneName;
+  public string exploreSceneName;
 
-    public void OnSkipClicked()
-    {
-        if (!string.IsNullOrEmpty(skipSceneName))
-            SceneManager.LoadScene(skipSceneName);
-        else
-            Debug.LogWarning("Skip scene not assigned in UIController.");
-    }
+  public void OnSkipClicked()
+  {
+    if (!string.IsNullOrEmpty(skipSceneName))
+      SceneManager.LoadScene(skipSceneName);
+    else
+      Debug.LogWarning("Skip scene not assigned in UIController.");
+  }
 
-    public void OnExploreClicked()
-    {
-        if (!string.IsNullOrEmpty(exploreSceneName))
-            SceneManager.LoadScene(exploreSceneName);
-        else
-            Debug.LogWarning("Explore scene not assigned in UIController.");
-    }
+  public void OnExploreClicked()
+  {
+    if (!string.IsNullOrEmpty(exploreSceneName))
+      SceneManager.LoadScene(exploreSceneName);
+    else
+      Debug.LogWarning("Explore scene not assigned in UIController.");
+  }
+
+  public void OnExitClicked()
+  {
+    Application.Quit();
+  }
+
+  public void backButtonClicked()
+  {
+    SceneManager.LoadScene("Shiva-Intro");
+  }
 }
